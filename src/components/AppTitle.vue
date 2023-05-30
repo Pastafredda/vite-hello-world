a40d9bdd3f07888e2f6756787307d0aaff429950
Pastafredda <justspotify09@gmail.com>
2023-05-31 00:50:23 +0200

    esercizio + bonus

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+    name: "AppTitle"
+}
+</script>
+  
+<template>
+    <section>
+        <h1>La mia prima app con Vite!</h1>
+
+    </section>
+</template>
+  
+<style scoped>
+h1 {
+    color: red;
+}
+</style>
---
+++ src/components/Jumbotron.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+    name: "Jumbotron"
+}
+</script>
+  
+<template>
+    <section>
+        <div class="jumbo">
+            <p>Il mio jumbotron</p>
+        </div>
+    </section>
+</template>
+  
+<style scoped>
+.jumbo {
+    background-color: white;
+    color: black;
+    width: 1000px;
+    height: 200px;
+}
+</style>
